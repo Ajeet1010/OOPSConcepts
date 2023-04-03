386d57c54d51a655a3071bb72e0f032dba421119
Ajeet <ajeetpal101999@gmail.com>
2023-04-03 23:12:00 +0530

    [Ajeet] Object & class created.

--- OOPSConcept/Program.cs
+++ OOPSConcept/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*****  OOPS CONCEPTS  *****");
+            // UC1- Creating an object for created class.
+            Student student = new Student();
+            student.Id = 1;
+            student.Name = "    Ajeet Pal";
+            Console.WriteLine(student.Id + student.Name);
         }
     }
 }
