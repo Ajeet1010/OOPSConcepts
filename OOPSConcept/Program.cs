38ab41cbd1b6ecbec662cddf19bea65a8874a89e
Ajeet <ajeetpal101999@gmail.com>
2023-04-05 00:38:35 +0530

    [Ajeet] Encapsulation.

--- OOPSConcept/Program.cs
+++ OOPSConcept/Program.cs
@@ -5,22 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*****  OOPS CONCEPTS  *****");
-
-
-            // dynamic polymorphism object
-            Console.WriteLine("\nDynamic Polymorphism");
-            Department dynamic = new Department();
-            dynamic.Stream();
-            dynamic = new Civil();
-            dynamic.Stream();
-            dynamic = new Mechanical();
-            dynamic.Stream();
-
-            // static dynamic polymorphism
-            Console.WriteLine("\n\nStatic Polymorphism");
-            StaticDepartment poly = new StaticDepartment();
-            poly.Student("Ajeet", "CSE");
-            poly.Student("Ajeet", "CSE", 19);
+            // encapsulation object
+            Encapsulation encapsulation = new Encapsulation();
+            encapsulation.SittingPlan();
+            //encapsulation.Encap();
 
         }
     }
